8e3d852d550ddb4e3b4c8fe6d90c1bd962d07a5d
Chinmay Pendharkar <notthetup@gmail.com>
2015-08-16 17:19:40 +0800

    cleaned up

--- update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-foreman run curl --data "secret=${WEBUILD_API_SECRET}" http://localhost:4000/api/v1/events/update
-foreman run curl --data "secret=${WEBUILD_API_SECRET}" http://localhost:4000/api/v1/repos/update
